39502e8d37ce61c58b28d9a4eaeaef8c0ce50760
aniyaDyx <930505227@qq.com>
2022-07-05 18:54:41 +0800

    dengyuxiang 电影页面终稿1

--- egg/app/controller/movie.js
+++ egg/app/controller/movie.js
@@ -9,12 +9,11 @@ class UserController extends Controller {
     let movieType = ctx.request.query.movieTypeSelect || ''
     let movieArea = ctx.request.query.areaSelect || ''
     let movieYear = ctx.request.query.ageSelect || ''
+    let timeSort = ctx.request.query.timeSort == 'timeSort' ? 'order by movie_time desc' : ''
+    let scoreSort = ctx.request.query.scoreSort == 'scoreSort' ? 'order by score desc' : ''
 
 
-
-
-
-    const sql = `select * from movie where movie_flag=${type} and (movie_type="${movieType}" or "${movieType}"="")  and  (movie_area="${movieArea}" or "${movieArea}"="") and (movie_year="${movieYear}" or "${movieYear}"="")`
+    const sql = `select * from movie where movie_flag=${type} and (movie_type="${movieType}" or "${movieType}"="")  and  (movie_area="${movieArea}" or "${movieArea}"="") and (movie_year="${movieYear}" or "${movieYear}"="") ${timeSort} ${scoreSort}`
 
     console.log(sql)
     let list = []
